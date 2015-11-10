4a7d6a9877b8b6816292ba60abd73640ec50b19a
Jessica Ro <jessro95@uw.edu>
2015-11-10 08:12:27 -0800

    Final

--- README.md
+++ README.md
@@ -3,7 +3,13 @@ This template is an optional starting point for a challenge for the following [c
 
 
 What is the purpose of your project (i.e., why would someone use this app)?
+The purpose of this project is to look for a artist, and see his/her related artists and top tracks.
+
 What is the URL of this project on your student web-server?
+
 Did you receive help from any other sources (classmates, etc.)? If so, please list who.
+I received help from Daniel Hoang and Mike
 Approximately how many hours did it take you to complete this challenge?
+24
 Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better?
+	I was stuck on ng-click for couple hours because I wasn't supposed to put {{}} around and it would be nice if that part was warned.
--- app.js
+++ app.js
@@ -1,15 +1,50 @@
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var topTrack;
+var relatedArtist;
+var baseUrl = 'https://api.spotify.com/v1/'
 var myApp = angular.module('myApp', [])
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
-  $scope.audioObject = {}
-  $scope.getSongs = function() {
-    $http.get(baseUrl + $scope.track).success(function(response){
-      data = $scope.tracks = response.tracks.items
-      
+  $scope.audioObject = {};
+
+  $scope.getArtist = function() {
+    $http.get(baseUrl + 'search?type=artist&query=' + $scope.artist).success(function(response){
+      data = $scope.artists = response.artists.items;
+      /*console.log(data[0].album.images[0].url);*/
     })
   }
+
+  $scope.getSongs = function(artistId) {
+    $http.get(baseUrl + 'artists/' + artistId + '/top-tracks?country=US').success(function(response){
+      $scope.topTracks = response.tracks;
+    })
+  }
+
+  $scope.getRelatedArtists = function(artistId) {
+    $http.get(baseUrl + 'artists/' + artistId + '/related-artists').success(function(response){
+      relatedArtist = $scope.relatedArtists = response.artists;
+    })
+  }
+
+  $scope.getInfo = function(artistId) {
+    $http.get(baseUrl + 'artists/' + artistId).success(function(response){
+      var imageUrl;
+      var genre;
+      var name; 
+      imageUrl = response.images[0].url;
+      genre = "" + response.genres;
+      name = response.name;
+
+      console.log(genre);
+      $('.artist-info').empty();
+      $(".artist-info").append('<img class = squareImage src = "' + imageUrl + '"> </img>');
+      $('.artist-info').append('<h3>' + name + '</h3>');
+      if(genre.length > 0) {
+        $('.artist-info').append('<h4> genre : ' + genre + '<h4>');
+      }
+    })
+  }
+
   $scope.play = function(song) {
     if($scope.currentSong == song) {
       $scope.audioObject.pause()
